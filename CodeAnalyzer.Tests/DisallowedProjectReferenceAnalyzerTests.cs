39c1d6ff9f86b63493a131da6befe7c508312267
Elan Hasson <elan@elanhasson.com>
2019-03-27 16:39:07 -0400

    Old style test

---
+++ CodeAnalyzer.Tests/DisallowedProjectReferenceAnalyzerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using CodeAnalyzer.Test;
+using CodeAnalyzer.Test.Helpers;
+using CodeAnalyzer.Tests.Helpers;
+
+namespace CodeAnalyzer.Tests
+{
+    [TestFixture]
+    [Category("Unit")]
+    public class DisallowedProjectReferenceAnalyzerTests : CodeFixVerifier
+    {
+        [Test]
+        public void NoDisallowedReferencesConfig_ShouldThrowDiagnosticErrorAboutMissingConfigFile()
+        {
+            var projectOptions = new ProjectOptions()
+            {
+                MetadataReferences = new MetadataReference[]
+                    {MetadataReference.CreateFromFile(typeof(JsonConvert).Assembly.Location),},
+                AdditionalDocuments = new AdditionalDocuments[]
+                    {new AdditionalDocuments("disallowed-references.yml", 
+                        content: @"- Newtonsoft.Json"),}
+            };
+
+            var test = @"
+            using System;
+            using Newtonsoft.Json;
+
+            namespace ConsoleApplication1
+            {
+                class CategoryDefinedTest
+                {   
+                    var x = JsonConvert.False;
+                }
+            }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "DisallowedReference",
+                Message = "The project '{0}' has a reference to 'Newtonsoft.Json', this is not allowed.",
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 17, 33)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, projectOptions, expected);
+        }
+
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return null;
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DisallowedProjectReferenceAnalyzer();
+        }
+    }
+}
---
+++ CodeAnalyzer/DisallowedProjectReferenceAnalyzer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+
+namespace CodeAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DisallowedProjectReferenceAnalyzer : DiagnosticAnalyzer
+    {
+        private static DiagnosticDescriptor DisallowedReference { get; } =
+            new DiagnosticDescriptor(
+                "DisallowedReference",
+                "Disallowed Reference",
+                "The project '{0}' has a reference to '{1}', this is not allowed.",
+                category: "Maintainability",
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+            ImmutableArray.Create(DisallowedReference);
+
+        public override void Initialize(AnalysisContext analysisContext)
+        {
+            analysisContext.RegisterCompilationAction(StartAction);
+        }
+
+        private void StartAction(CompilationAnalysisContext compilationAnalysisContext)
+        {
+            ImmutableArray<AdditionalText> additionalFiles = compilationAnalysisContext.Options.AdditionalFiles;
+            AdditionalText configFile = additionalFiles.FirstOrDefault(file =>
+            {
+                if (file?.Path == null)
+                {
+                    throw new NullReferenceException($"{nameof(file)} is passed as null via AdditionalFiles.");
+                }
+                return Path.GetFileName(path: file.Path)
+                           .Equals("disallowed-references.yml", StringComparison.InvariantCultureIgnoreCase);
+            });
+
+
+            HashSet<string> disallowedReferences = new HashSet<string>();
+            if (configFile == null)
+            {
+                Console.WriteLine("disallowed-references.yml not found, make sure you have it set as an AdditionalFile.");
+            }
+
+            string hardCodedListMessage = string.Empty;
+            if (configFile != null)
+            {
+                // Read the file line-by-line to get the terms.
+                SourceText fileText = configFile.GetText(compilationAnalysisContext.CancellationToken);
+                MemoryStream stream = new MemoryStream();
+                using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8, 1024, true))
+                {
+                    fileText.Write(writer);
+                }
+
+                stream.Position = 0;
+                var deserializer = new Deserializer();
+                using (var reader = new StreamReader(stream))
+                {
+                    var configFileDeserialize = deserializer.Deserialize<List<string>>(reader);
+
+
+                    foreach (var assemblyName in configFileDeserialize)
+                    {
+                        disallowedReferences.Add(assemblyName.Trim());
+                    }
+                }
+            }
+
+            foreach (var disallowedReference in disallowedReferences)
+            {
+                Console.WriteLine($"Disallowed Assembly Name: {disallowedReference}");
+            }
+
+
+            // Check every named type for the invalid references.
+            var compilation = compilationAnalysisContext.Compilation;
+
+            foreach (var compilationReferencedAssemblyName in compilation.ReferencedAssemblyNames)
+            {
+                if (disallowedReferences.Contains(compilationReferencedAssemblyName.Name))
+                {
+                    compilationAnalysisContext.ReportDiagnostic(
+                        Diagnostic.Create(
+                            DisallowedReference,
+                            null,
+                            compilation.AssemblyName,
+                            compilationReferencedAssemblyName.Name));
+                }
+            }
+        }
+    }
+}
