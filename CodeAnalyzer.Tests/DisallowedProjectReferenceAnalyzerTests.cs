663d83a8d13e9d06cc756f8c5d1e99bdef980882
Sam Harwell <sam@tunnelvisionlabs.com>
2019-03-27 16:24:38 -0500

    Update to Microsoft.CodeAnalysis.Testing

--- CodeAnalyzer.Tests/DisallowedProjectReferenceAnalyzerTests.cs
+++ CodeAnalyzer.Tests/DisallowedProjectReferenceAnalyzerTests.cs
@@ -1,32 +1,22 @@
-﻿using System;
+﻿using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
 using Newtonsoft.Json;
 using NUnit.Framework;
-using CodeAnalyzer.Test;
-using CodeAnalyzer.Test.Helpers;
-using CodeAnalyzer.Tests.Helpers;
+using VerifyCS = Microsoft.CodeAnalysis.CSharp.Testing.NUnit.CodeFixVerifier<CodeAnalyzer.DisallowedProjectReferenceAnalyzer, Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Microsoft.CodeAnalysis.CSharp.Testing.NUnit.CodeFixVerifier<CodeAnalyzer.DisallowedProjectReferenceAnalyzer, Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
 
 namespace CodeAnalyzer.Tests
 {
     [TestFixture]
     [Category("Unit")]
-    public class DisallowedProjectReferenceAnalyzerTests : CodeFixVerifier
+    public class DisallowedProjectReferenceAnalyzerTests
     {
         [Test]
-        public void NoDisallowedReferencesConfig_ShouldThrowDiagnosticErrorAboutMissingConfigFile()
+        public async Task NoDisallowedReferencesConfig_ShouldThrowDiagnosticErrorAboutMissingConfigFile()
         {
-            var projectOptions = new ProjectOptions()
-            {
-                MetadataReferences = new MetadataReference[]
-                    {MetadataReference.CreateFromFile(typeof(JsonConvert).Assembly.Location),},
-                AdditionalDocuments = new AdditionalDocuments[]
-                    {new AdditionalDocuments("disallowed-references.yml", 
-                        content: @"- Newtonsoft.Json"),}
-            };
-
-            var test = @"
+            var source = @"
             using System;
             using Newtonsoft.Json;
 
@@ -38,30 +28,22 @@ namespace CodeAnalyzer.Tests
                 }
             }";
 
-            var expected = new DiagnosticResult
+            var test = new Microsoft.CodeAnalysis.CSharp.Testing.CSharpCodeFixTest<DisallowedProjectReferenceAnalyzer, EmptyCodeFixProvider, NUnitVerifier>
             {
-                Id = "DisallowedReference",
-                Message = "The project '{0}' has a reference to 'Newtonsoft.Json', this is not allowed.",
-                Severity = DiagnosticSeverity.Error,
-                Locations =
-                    new[]
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { MetadataReference.CreateFromFile(typeof(JsonConvert).Assembly.Location) },
+                    AdditionalFiles = { ("disallowed-references.yml", "- Newtonsoft.Json") },
+                    ExpectedDiagnostics =
                     {
-                        new DiagnosticResultLocation("Test0.cs", 17, 33)
-                    }
+                        VerifyCS.Diagnostic().WithArguments("TestProject", "Newtonsoft.Json"),
+                        DiagnosticResult.CompilerError("CS0825").WithSpan(9, 21, 9, 24).WithMessage("The contextual keyword 'var' may only appear within a local variable declaration or in script code"),
+                    },
+                }
             };
 
-            VerifyCSharpDiagnostic(test, projectOptions, expected);
-        }
-
-
-        protected override CodeFixProvider GetCSharpCodeFixProvider()
-        {
-            return null;
-        }
-
-        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
-        {
-            return new DisallowedProjectReferenceAnalyzer();
+            await test.RunAsync();
         }
     }
 }
